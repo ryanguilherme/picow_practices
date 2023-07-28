3f01deb8b243a24980b9832d489ccef89ef3eb92
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-07-27 23:28:27 -0300

    blink 2 leds when push a button

--- button/button.c
+++ button/button.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+void led_init(uint GPIO) {
+    gpio_init(GPIO);
+    gpio_set_dir(GPIO, GPIO_OUT);
+}
 
+void button_init(uint GPIO) {
+    gpio_init(GPIO);
+    gpio_set_dir(GPIO, GPIO_IN);
+}
+
+/* GPIO VALUE DECLARATION */
+const uint BUTTON1 = 22;
+const uint LED1    = 27;
+const uint LED2    = 28;
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    button_init(BUTTON1);
+    led_init(LED1);
+    led_init(LED2);
+
+    while (1) {
+        uint led1_flag = gpio_get(LED1);
+        uint led2_flag = gpio_get(LED2);
+        if (gpio_get(BUTTON1)) {
+            gpio_put(LED1, (led1_flag ^= 1));
+            gpio_put(LED2, (led2_flag ^= 1));
+        }
+    }
 }
