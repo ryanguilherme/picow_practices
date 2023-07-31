a745b0b73b659860a7c4caf8e3a5728ca2b8228c
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-07-30 22:37:08 -0300

    alarm done but not tested yet

--- alarm/alarm.c
+++ alarm/alarm.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include "pico/stdlib.h"
+
+/* GPIO PORT DEFINITIONS */
+#define PIR_IN     28
+#define BUZZER_OUT 20
 
 int main() {
-    printf("Hello, World!\n");
+    gpio_init(PIR_IN);
+    gpio_init(BUZZER_OUT);
+    gpio_set_dir(PIR_IN, GPIO_IN);
+    gpio_set_dir(BUZZER_OUT, GPIO_OUT);
+    while (1) {
+        if (gpio_get(PIR_IN)) {
+            if (!gpio_get(BUZZER_OUT))
+                gpio_put(BUZZER_OUT, 1);
+        }
+        else {
+            if (gpio_get(BUZZER_OUT))
+                gpio_put(BUZZER_OUT, 0);
+        }
+    }
     return 0;
 }
